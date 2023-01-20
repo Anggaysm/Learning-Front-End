f1d2863eceaebbddb37a285fbbed8ff96f62fc2a
Anggaysm <prasetiaangga28@gmail.com>
2023-01-20 19:36:05 +0700

    satu

---
+++ holadok1.1/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "DM sans": ["Dm sans"],
+      },
+      colors: {
+        tulisan: "#101C45",
+        blok: "#203271",
+      },
+    },
+  },
+  plugins: [],
+};
