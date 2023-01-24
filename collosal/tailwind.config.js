d96ce9041f48673324d566fe83d08ad426c39588
Anggaysm <prasetiaangga28@gmail.com>
2023-01-24 17:49:54 +0700

    setengah home

--- collosal/tailwind.config.js
+++ collosal/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nosans: ["Noto sans", "sans serif"],
+      },
+      colors: {
+        primary: "#6016FC",
+        ijo: "#16FCD2",
+        highligth: "#221048",
+        gelap: "#0B0B22",
+        mermud: "#FC165B",
+      },
+    },
   },
   plugins: [],
 };
